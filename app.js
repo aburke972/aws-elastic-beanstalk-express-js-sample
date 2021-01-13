f4980003c5de772d72cba8121c77dd997855abfd
aburke972 <axel.burke@gmail.com>
2021-01-13 16:38:05 +0100

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Supa Kitty!'));
+app.get('/', (req, res) => res.send('Hello Supaaaaaaaa Kitty!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
